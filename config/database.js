e0879982d139f88c3a6c2056e7095102971b0c49
Apollo-otete <apollo2juniorm@gmail.com>
2026-03-12 12:50:32 +0300

    backendtest

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require ("mongoose")
 const connectDb =async()=>{
   try {
-    await mongoose.connect("mongodb+srv://apollo7brian_db_user:fXGxDSQ13qJcjiRD@cluster0.zsfpsep.mongodb.net/?appName=Cluster0",{})
+    await mongoose.connect("mongodb+srv://apollos:bagy9541@cluster0.bpidegm.mongodb.net/?appName=Cluster0",{})
     console.log("data connected succesfully")
   } catch (error) {
     console.log(error)
